7d9c096134450e822d879e081d98d5ee2e96e290
Air <Air@Air.local>
2019-08-20 19:45:05 +0800

    Add burger display

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './burgerIngredient.css';
+import classes from './BurgerIngredient.css';
 
 const burgerIngredient = (props) => {
   let ingredient = null;
